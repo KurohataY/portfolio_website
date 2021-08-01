1ce59246740ea54e6b060888b883f36fd79dd82d
KurohataY <shihata001@gmail.com>
2021-08-01 17:17:08 +0900

    ブログ移行作業（グローバルナビ作成中）

---
+++ components/blog/nav/navbar.vue
@@ -0,0 +1,238 @@
+<template lang="">
+    <div>
+        <nav>
+            <ul>
+                <li v-for="(category, index) in categorys" :key="index" :class="{'animate': menuClick}">
+                    <v-tooltip bottom>
+                        <template v-slot:activator="{ on, attrs }">
+                            <nuxt-link :to="{path: category.link}">
+                                <v-icon 
+                                    v-bind="attrs"
+                                    v-on="on" 
+                                    class="material-icons icon_style">
+                                    {{category.iconName}}
+                                </v-icon>
+                            </nuxt-link>
+                        </template>
+                        <span>{{category.name}}</span>
+                    </v-tooltip>
+                </li>
+            </ul>
+        </nav>
+        <div class="nav-container" :class="{'nav-open': menuClick}"><div class="menu-icon" :class="{'open': menuClick}" v-on:click="toggleMenu"><span></span></div></div>  
+    </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    menuClick: false,
+    categorys: [
+      { name: "トップ", iconName: "home", link: "/blog" },
+      {
+        name: "プログラミング",
+        iconName: "code",
+        link: "/blog?category=programming",
+      },
+      { name: "IT", iconName: "computer", link: "/blog?category=it" },
+      { name: "日記", iconName: "auto_stories", link: "/blog?category=daily" },
+      { name: "サイトマップ", iconName: "view_list", link: "/blog/site-map" },
+      {
+        name: "プライバシーポリシー",
+        iconName: "policy",
+        link: "/blog/privacy-policy",
+      },
+    ],
+  }),
+  methods: {
+    toggleMenu() {
+      this.menuClick = !this.menuClick;
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+@mixin longshadow($color) {
+  text-shadow: makelongshadow($color);
+}
+
+@mixin transition($property: all, $delay: false) {
+  -moz-transition: $property;
+  -o-transition: $property;
+  -webkit-transition: $property;
+  -webkit-transition-delay: $delay;
+  transition: $property;
+}
+
+@mixin box-sizing($sizing) {
+  -moz-box-sizing: $sizing;
+  -webkit-box-sizing: $sizing;
+  box-sizing: $sizing;
+}
+*,
+*::before,
+*::after {
+  @include box-sizing(border-box);
+}
+
+$shadowlength: 60;
+
+@function makelongshadow($color) {
+  $val: 0px 0px $color;
+  @for $i from 1 through $shadowlength {
+    $val: #{$val}, #{$i}px #{$i}px #{$color};
+  }
+  @return $val;
+}
+
+html,
+body {
+  background: #111;
+  height: 100%;
+}
+
+.nav-container {
+  background: #eff0f1;
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  @include transition(all 0.3s ease 0.6s);
+  z-index: 10;
+  padding: 1em;
+  position: relative;
+}
+
+.nav-container.nav-open {
+  margin-top: 8em;
+  @include transition(all 0.3s ease);
+}
+
+.menu-icon {
+  position: relative;
+  display: block;
+  width: 2.5em;
+  height: 2.5em;
+  cursor: pointer;
+
+  &:hover span {
+    background: #222;
+
+    &:before,
+    &:after {
+      background: #222;
+    }
+  }
+}
+
+.menu-icon > span {
+  position: absolute;
+  top: 50%;
+  display: block;
+  width: 100%;
+  height: 0.25em;
+  background-color: lighten(#222, 60%);
+  border-radius: 3px;
+  @include transition(transform 0.3s, 0.25s ease);
+}
+
+.menu-icon > span:before,
+.menu-icon > span:after {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: lighten(#222, 60%);
+  border-radius: 3px;
+  @include transition(transform 0.3s, 0.25s ease);
+}
+
+.menu-icon > span:before {
+  transform: translateY(-0.75em);
+}
+
+.menu-icon > span:after {
+  transform: translateY(0.75em);
+}
+
+.menu-icon.open {
+  -webkit-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+
+.menu-icon.open > span:before {
+  -webkit-transform: rotate(90deg);
+  transform: rotate(90deg);
+}
+
+.menu-icon.open > span:after {
+  -webkit-transform: rotate(90deg);
+  transform: rotate(90deg);
+}
+
+nav {
+  position: fixed;
+  top: 0;
+  //   left: 20%;
+  color: black;
+  z-index: 1;
+  width: 100%;
+  text-align: center;
+
+  ul {
+    li.animate {
+      top: 0;
+    }
+
+    li {
+      display: inline-block;
+      text-align: center;
+      margin: 1em 2em;
+      cursor: pointer;
+      position: relative;
+      top: -8em;
+      $speed: 0.3s;
+      $icons: 3;
+
+      &:nth-child(1) {
+        @include transition(top $speed ease 0.15s);
+      }
+      &:nth-child(2) {
+        @include transition(top $speed ease 0.2s);
+      }
+      &:nth-child(3) {
+        @include transition(top $speed ease 0.25s);
+      }
+      &:nth-child(4) {
+        @include transition(top $speed ease 0.3s);
+      }
+      &:nth-child(5) {
+        @include transition(top $speed ease 0.35s);
+      }
+      &:nth-child(6) {
+        @include transition(top $speed ease 0.4s);
+      }
+      &:nth-child(7) {
+        @include transition(top $speed ease 0.45s);
+      }
+
+      &:hover {
+        opacity: 1;
+      }
+
+      .icon_style {
+        font-size: 1.5em;
+        background: #4fc3f7;
+        padding: 0.75em;
+        border-radius: 50%;
+        color: black;
+        display: inline-block;
+        overflow: hidden;
+        @include longshadow(rgba(41, 182, 246, 1));
+      }
+
+      h4 {
+        line-height: 2.5;
+      }
+    }
+  }
+}
+</style>
--- components/profile/profile-contents.vue
+++ components/profile/profile-contents.vue
@@ -4,11 +4,11 @@
       <v-list-item three-line>
         <v-list-item-content>
           <v-list-item-title class="headline mb-1">
-            <h1>Profile</h1>
+            <h1>{{profile_title}}</h1>
           </v-list-item-title>
         </v-list-item-content>
         <v-list-item-avatar tile size="150" color="grey">
-          <v-img src="images/profile.webp"></v-img>
+          <v-img :src="avatar_img_path"></v-img>
         </v-list-item-avatar>
       </v-list-item>
       <TextAnimetion />
@@ -22,6 +22,10 @@ export default {
   components: {
     TextAnimetion,
   },
+  data: () => ({
+    avatar_img_path: "images/profile.webp",
+    profile_title: "Profile",
+  }),
 };
 </script>
 
