c74a1ac12dc15f8e69c6a7a0f1727232e15244f4
KurohataY <shihata001@gmail.com>
2021-08-11 00:06:58 +0900

    Google Code Prettify対応＋画像サイズ位置調整

--- components/blog/ui/contents/content.vue
+++ components/blog/ui/contents/content.vue
@@ -1,7 +1,7 @@
 <template lang="">
     <div>
         <h1>{{title}}</h1>
-        <div v-for="bc in blogContent" :key="bc.fluid">
+        <div v-for="(bc, index) in blogContent" :key="index">
             <div v-html="bc.content"></div>
             <div v-html="bc.html"> </div>
         </div>
@@ -9,11 +9,25 @@
 </template>
 <script>
 export default {
-    props: {
-        title: String, 
-        blogContent: Object,
-    },
+  props: {
+    title: String,
+    blogContent: Array,
+  },
+  mounted() {
+    // https://github.com/google/code-prettify/issues/578
+    // これを行わないとうまく反映されない
+    const prettyPrint = require("code-prettify");
+    this.$nextTick(function () {
+      prettyPrint.prettyPrint();
+    });
+  },
 };
 </script>
-<style lang="">
+<style lang="scss">
+img {
+  width: 80%;
+  text-align: center;
+  margin: 0 auto;
+  display: block;
+}
 </style>
