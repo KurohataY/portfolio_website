e01cb88f7b625e6904a8891e8534bdd87a9423d0
KurohataY <shihata001@gmail.com>
2021-08-14 18:23:20 +0900

    コード修正

--- components/blog/post/post.vue
+++ components/blog/post/post.vue
@@ -1,51 +1,26 @@
 <template lang="">
-    <div class="pa-4 post">
-        <h1>{{title}}</h1>
-        <ul class="lists">
-          <li :class="`list ${toc.name}`" v-for="toc in toc" :key="toc.id">
-          <n-link v-scroll-to="`#${toc.id}`" to>
-            {{ toc.text }}
-          </n-link>
-          </li>
-        </ul>
-        <div v-for="(bc, index) in blogContent" :key="index">
-            <div v-html="bc.content"></div>
-            <div v-html="bc.html"> </div>
-        </div>
-    </div>
+
+   <v-container class="pa-4 post">
+      <h1>{{title}}</h1>
+      <ul class="toc_lists">
+        <li :class="`list ${toc.name}`" v-for="toc in toc" :key="toc.id">
+        <n-link v-scroll-to="`#${toc.id}`" to>
+          {{ toc.text }}
+        </n-link>
+        </li>
+      </ul>
+      <div v-for="(bc, index) in blogContent" :key="index">
+          <div v-html="bc.content"></div>
+          <div v-html="bc.html"> </div>
+      </div>
+    </v-container>
 </template>
 <script>
-import cheerio from "cheerio";
-
 export default {
   props: {
     title: String,
     blogContent: Array,
-  },
-  data() {
-    return {
-      toc: [],
-    };
-  },
-  created() {
-    // console.log(this.blogContent);
-    this.toc = this.tableOfContents();
-  },
-  methods: {
-    tableOfContents() {
-      var body = "";
-      for (let i = 0; i < this.blogContent.length; i++) {
-        body = body + this.blogContent[i].content;
-      }
-      const $ = cheerio.load(body);
-      const headings = $("h1, h2, h3").toArray();
-      const toc = headings.map((data) => ({
-        text: data.children[0].data,
-        id: data.attribs.id,
-        name: data.name,
-      }));
-      return toc;
-    },
+    toc: Array,
   },
   mounted: function () {
     // https://github.com/google/code-prettify/issues/578
@@ -55,11 +30,15 @@ export default {
   },
 };
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 img {
   width: 80%;
   text-align: center;
   margin: 0 auto;
   display: block;
+}
+.toc_lists li a {
+  text-decoration: none;
+  color: black;
 }
 </style>
