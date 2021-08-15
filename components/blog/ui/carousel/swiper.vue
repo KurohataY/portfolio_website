c29abdbcc8942b8db13b32e51b9e83835c224b48
KurohataY <shihata001@gmail.com>
2021-08-16 01:14:59 +0900

    レスポンシブ対応

--- components/blog/ui/carousel/swiper.vue
+++ components/blog/ui/carousel/swiper.vue
@@ -7,7 +7,7 @@
           <nuxt-link :to="'/blog/' + content.id">
             <v-img
               :src="content.thumbnail.url"
-              width="300px"
+              width="100%"
               height="250px"
               class="white--text align-end"
               gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
@@ -29,12 +29,23 @@ export default {
   data() {
     return {
       swiperOption: {
-        slidesPerView: 4,
+        breakpoints: {
+          960: {
+            slidesPerView: 4,
+            spaceBetween: 5,
+          },
+          600: {
+            slidesPerView: 3,
+          },
+          300: {
+            slidesPerView: 1,
+          },
+        },
         speed: 1000, //スライドの切り替わりスピード
         // spaceBetween: 30, //各スライドの余白
         centeredSlides: true, //スライダーを真ん中に
         loop: true, //無限ループ
-        spaceBetween: 5,
+
         autoplay: {
           //スライドの自動切り替え
           delay: 2500, //スライドの自動切り替えの秒数
