10ff089772617b5c95aeba7f498d60aa8ff26722
KurohataY <shihata001@gmail.com>
2021-08-25 01:13:02 +0900

    プロフィールカード作成中

---
+++ components/blog/ui/profile/profile.vue
@@ -0,0 +1,49 @@
+
+<template>
+  <v-card class="mx-auto" style="text-align: center">
+    <v-avatar class="profile" color="grey" size="164" rounded>
+      <v-img :src="imgUrl"></v-img>
+    </v-avatar>
+
+    <v-card-title style="display: block"> {{ profileName }} </v-card-title>
+
+    <v-card-subtitle> 趣味：漫画、アニメ鑑賞、温泉旅行 </v-card-subtitle>
+    <v-btn
+      icon
+      color="light-blue"
+      :href="twitterUrl"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <v-icon>mdi-twitter</v-icon>
+    </v-btn>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn icon @click="show = !show">
+        <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text v-html="profileText"> </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+      imgUrl: "/favicon.ico",
+      profileName: "イザナギ",
+      profileText:
+        "日々学習したことや気になったことを気まぐれに投稿しています。<br>趣味ブログ：https://izanagi-craft.com/",
+      twitterUrl: process.env.TWITTER_URL,
+    };
+  },
+};
+</script>
